8667f5aeb9a6018a11bc3f7e68ca20e1430dbbf6
joshua oxner <joshuaoxner@joshuas-MacBook-Pro.local>
2020-06-09 20:06:25 -0600

    First of many im sure

---
+++ config/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'Joshua', // Navigation and Site Title
+  siteTitleAlt: 'Joshua - Portfolio', // Alternative Site title for SEO
+  siteTitleShort: 'Joshua', // short_name for manifest
+  siteHeadline: 'Come & Enjoy our excellent photos', // Headline for schema.org JSONLD
+  siteUrl: 'https://jodie.lekoarts.de', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logos/logo.png', // Used for SEO and manifest
+  siteDescription: 'Full Stack Portfolio App To Get To Know ME',
+  author: 'Joshua Oxner', // Author for schema.org JSONLD
+
+  ogLanguage: 'en_US', // og:language
+  googleAnalyticsID: 'UA-XXXXXX-X',
+
+  // Manifest and Progress color
+  themeColor: '#db7436',
+  backgroundColor: '#3b3c4f',
+}
